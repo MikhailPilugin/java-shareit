5b5910c2808c25600486ebb8355a3b99ae9571f9
Mikhail Pilugin <tonopod8@yandex.ru>
2023-06-30 23:08:09 +0300

    Codestyle правки - проверки в git

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -4,19 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.exceptions.ErrorResponse;
-import ru.practicum.shareit.exceptions.ValidationException;
-import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.User;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 import java.util.Collection;
 import java.util.List;
-
-/**
- * TODO Sprint add-controllers.
- */
 
 @RestController
 @RequiredArgsConstructor
@@ -31,25 +23,25 @@ public class ItemController {
 
     @GetMapping("/{itemId}")
     public Item get(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                   @PathVariable Long itemId) {
+                    @PathVariable Long itemId) {
         return itemService.getById(userId, itemId);
     }
 
     @GetMapping("/search")
     public List<Item> search(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                @RequestParam(required = true) String text) {
+                             @RequestParam(required = true) String text) {
         return itemService.search(userId, text);
     }
 
     @PostMapping
     public Item add(@RequestHeader("X-Sharer-User-Id") Long userId, @Valid
-                    @RequestBody Item item) {
+    @RequestBody Item item) {
         return itemService.add(userId, item);
     }
 
     @PatchMapping("/{itemId}")
     public Item update(@RequestHeader("X-Sharer-User-Id") Long userId,
-                          @RequestBody Item item, @PathVariable Integer itemId) {
+                       @RequestBody Item item, @PathVariable Integer itemId) {
         return itemService.update(itemId, item, userId);
     }
 
